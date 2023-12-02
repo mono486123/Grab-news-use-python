13f80627afe78869b959d304a5c879b9b07f21b4
mono486123 <115555813+mono486123@users.noreply.github.com>
2023-12-02 23:08:38 +0800

    Update yahoo新聞.py

--- yahoo新聞.py
+++ yahoo新聞.py
@@ -1,27 +1,18 @@
-import re
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 
-#options = webdriver.ChromeOptions()
-#options.add_argument('--headless')
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
 
 
 
 
-input_text = "經濟"
+input_text = input("輸入文字:" )
 
-# 正則表達
-match = re.search(fr"({input_text})", input_text)
-
-if match:
-    search_keyword = match.group()
-else:
-    search_keyword = None
-
-if search_keyword:
-    driver = webdriver.Chrome()#options=options) 
+if input_text:
+    driver = webdriver.Chrome(options=options) 
 
 
     driver.get("https://tw.news.yahoo.com/")
@@ -30,51 +21,39 @@ if search_keyword:
     search_box = driver.find_element(By.XPATH, '//*[@id="ybar-sbq"]')
     
     # 在搜尋框中輸入搜尋內容
-    search_box.send_keys(search_keyword)
+    search_box.send_keys(input_text)
     search_box.send_keys(Keys.RETURN)
-    
-
-   
-   ## 模擬滾動頁面，觸發更多新聞的加載
-
     time.sleep(5)
-
-
-    if search_box.send_keys(Keys.RETURN):
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-
-
 
    # # 等待一些時間，讓新聞加載完成
     driver.implicitly_wait(5)
+ 
+    titles = driver.find_elements(By.XPATH, '//h3/a')
+    links = driver.find_elements(By.XPATH, '//h3/a')
 
-    
+     # 建一个列表来保存匹配的结果
+    results = []
 
-    num_of_results = 40  # 設定想要獲取的資料數量
-    titles = driver.find_elements(By.CSS_SELECTOR, '#stream-container-scroll-template li div h3 a')[:num_of_results]
-    links = driver.find_elements(By.CSS_SELECTOR, '#stream-container-scroll-template li div h3 a')[:num_of_results]
-
-
-
-
-    
     for index, (title, link) in enumerate(zip(titles, links), start=1):
-        title_text = title.text
-        link_href = link.get_attribute("href")
-        # time = times[i].get_attribute("datetime")
-
-       # if not 'https://tw.news.yahoo.com/' in link_href:
-       #    continue
-       # 
-
-       
+        # Extract title information
+        title_text = title.text if title else "N/A"
+        results.append({'編號': index, '標題': title_text})
+    
+        # Extract link information
+        link_href = link.get_attribute("href") if link else "N/A"
+        results.append({'連結': link_href})
+    
+        # Print combined information
         print(f"編號: {index}")
-        print(f"標題: {title_text}\n")
+        print(f"標題: {title_text}")
         print(f"連結: {link_href}\n\n")
-        # print(f"時間: {time}\n")
+    
+        
+    
+    
 
-   
+
+
 
 else:
     print("未找到關鍵字")
